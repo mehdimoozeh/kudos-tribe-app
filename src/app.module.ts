2e5bea62d22b6cc83fcd418a56cb03c996c166b0
Mehdi <mohammadmoozeh@gmail.com>
2022-04-09 15:14:56 +0300

    refactor: disable logger middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,10 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
 import { TribeWebhookModule } from './tribe-webhook/tribe-webhook.module';
-import { LoggerMiddleware } from './middlewares/logger.middleware';
 
 @Module({
   imports: [ConfigModule.forRoot(), TribeWebhookModule],
   controllers: [AppController],
 })
-export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer): void {
-    consumer.apply(LoggerMiddleware).forRoutes('*');
-  }
-}
+export class AppModule {}
