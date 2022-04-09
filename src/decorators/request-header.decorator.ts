6be237f4587c1863caeaa7d3553e86233a7c5806
Mehdi <mohammadmoozeh@gmail.com>
2022-04-09 13:30:03 +0300

    feat: custom decorator for header validation

---
+++ src/decorators/request-header.decorator.ts
@@ -0,0 +1,27 @@
+import {
+  createParamDecorator,
+  ExecutionContext,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { plainToClass } from 'class-transformer';
+import { validate, ValidationError } from 'class-validator';
+
+export const RequestHeader = createParamDecorator(
+  async (value: any, ctx: ExecutionContext) => {
+    const headers = ctx.switchToHttp().getRequest().headers;
+    const dto = plainToClass(value, headers);
+
+    const errors: ValidationError[] = await validate(dto);
+    if (errors.length > 0) {
+      const validationErrors = errors.map((obj) =>
+        Object.values(obj.constraints),
+      );
+      throw new HttpException(
+        `Validation failed : ${validationErrors}`,
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+    return dto;
+  },
+);
