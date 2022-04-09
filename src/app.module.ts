88ebb722389a351531705b5a46854c5005d8979e
Mehdi <mohammadmoozeh@gmail.com>
2022-04-09 13:58:26 +0300

    feat(temporary): add request logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
 import { TribeWebhookModule } from './tribe-webhook/tribe-webhook.module';
+import { LoggerMiddleware } from './middlewares/logger.middleware';
 
 @Module({
   imports: [ConfigModule.forRoot(), TribeWebhookModule],
   controllers: [AppController],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): void {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
