5069595c64257016a56bb534584d5c95d2a061eb
Mehdi <mohammadmoozeh@gmail.com>
2022-04-09 13:27:47 +0300

    feat: add validation

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      skipMissingProperties: false,
+    }),
+  );
   const configService = app.get(ConfigService);
   const PORT = +configService.get('PORT');
   const logger = new Logger();
