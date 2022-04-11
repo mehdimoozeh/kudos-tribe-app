970db3109a94c6a80af0a94d6d32862e9d82aed0
Mehdi <mohammadmoozeh@gmail.com>
2022-04-11 18:15:48 +0300

    feat: kudos business logic

--- src/tribe-webhook/tribe-webhook.module.ts
+++ src/tribe-webhook/tribe-webhook.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TribeWebhookService } from './tribe-webhook.service';
 import { TribeWebhookController } from './tribe-webhook.controller';
+import { DatabaseService } from './database.service';
 
 @Module({
-  providers: [TribeWebhookService],
-  controllers: [TribeWebhookController]
+  providers: [TribeWebhookService, DatabaseService],
+  controllers: [TribeWebhookController],
+  exports: [DatabaseService],
 })
 export class TribeWebhookModule {}
--- src/tribe-webhook/tribe-webhook.service.ts
+++ src/tribe-webhook/tribe-webhook.service.ts
@@ -1,42 +1,39 @@
 import { Injectable, Logger } from '@nestjs/common';
 import type { TribeWebhooksBodyDto } from './dtos';
+import { DatabaseService } from './database.service';
 
 @Injectable()
 export class TribeWebhookService {
   private readonly KUDOS_EMOJI_UNICODE = '\u{1F369}'; // 🍩
-  private webhookIds = new Set<ID>();
   private readonly logger = new Logger(TribeWebhookService.name);
 
-  private saveDataId(dataId: ID): void {
-    this.webhookIds.add(dataId);
-    this.logger.log(`Saved! data.id: ${dataId}`);
-  }
-
-  private isDataIdExist(dataId: ID): boolean {
-    const result = this.webhookIds.has(dataId);
-    if (!result) this.saveDataId(dataId);
-    return result;
-  }
+  constructor(private readonly databaseService: DatabaseService) {}
 
   private detectEmojis(text: string): number {
     const emojis: string[] = text.match(/(\p{Emoji})+/gu) || [];
     this.logger.verbose(emojis);
-    emojis.forEach((item) => console.log(item, item.length));
+    // emojis.forEach((item) => console.log(item, item.length));
     // TODO: fix counting connected emojis like 🍩🍩, 🍩🤪
     return emojis.filter((item) => item === this.KUDOS_EMOJI_UNICODE).length;
   }
 
   public handleWebhookRequest(input: TribeWebhooksBodyDto) {
-    if (this.isDataIdExist(input.data.id)) {
+    if (this.databaseService.isDataIdExist(input.data.id)) {
       this.logger.log(`Duplicated webhook request ${input.data.id}`);
       return;
     }
-    const kudos = this.detectEmojis(input.data.object.shortContent);
-    const mentionedPeople = input.data.object.mentionedMembers;
+    const countedKudos = this.detectEmojis(input.data.object.shortContent);
+    const giver = input.data.object.createdById;
+    const mentionedPeople = input.data.object.mentionedMembers.filter(
+      (member) => member !== giver,
+    );
+    if (countedKudos === 0 || mentionedPeople.length === 0) return;
     this.logger.verbose({
-      kudos,
+      kudos: countedKudos,
       mentionedPeople,
     });
+    this.databaseService.save(giver, mentionedPeople, countedKudos);
+    this.databaseService.print();
     // console.log(input.data.object);
     // console.log(input.data.target);
     // Rest of logic
