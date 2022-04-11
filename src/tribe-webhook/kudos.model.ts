abbae690ea0d32a18abc964c63d2a43622744322
Mehdi <mohammadmoozeh@gmail.com>
2022-04-11 18:13:50 +0300

    feat: add kudos model

---
+++ src/tribe-webhook/kudos.model.ts
@@ -0,0 +1,26 @@
+export class KudosModel {
+  private member: ID;
+  private dayCapacity: number;
+  private remainingKudos: number;
+  private given: { [member: ID]: number }[] = [];
+  private received: { [member: ID]: number }[] = [];
+
+  constructor(member: ID) {
+    this.member = member;
+    this.dayCapacity = 5;
+    this.remainingKudos = this.dayCapacity;
+  }
+
+  give(receivers: ID[], count: number): number | Error {
+    const totalGiven = receivers.length * count;
+    if (totalGiven > this.remainingKudos)
+      throw new Error('You dont have enough Kudos!');
+    receivers.forEach((receiver) => this.given.push({ [receiver]: count }));
+    this.remainingKudos -= totalGiven;
+    return totalGiven;
+  }
+
+  receive(from: ID, count: number) {
+    this.received.push({ [from]: count });
+  }
+}
