bcc1318362e83365477aaa77ee2d1f0dde560742
Mehdi <mohammadmoozeh@gmail.com>
2022-04-13 21:19:14 +0300

    refactor(validation): disable debug message on production

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,13 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const configService = app.get(ConfigService);
   app.useGlobalPipes(
     new ValidationPipe({
       skipMissingProperties: false,
-      enableDebugMessages: true,
+      enableDebugMessages: configService.get('NODE_ENV') !== 'production',
     }),
   );
-  const configService = app.get(ConfigService);
   const PORT = +configService.get('PORT');
   const logger = new Logger();
   await app.listen(PORT, () => {
