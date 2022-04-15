490b4f027ba90d139e4c321b1df498636487a6f9
Mehdi <mohammadmoozeh@gmail.com>
2022-04-15 20:45:08 +0300

    refactor: post leaderboard

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -2,13 +2,20 @@ import { Injectable, Logger } from '@nestjs/common';
 import { TribeClient } from '@tribeplatform/gql-client';
 import { ConfigService } from '@nestjs/config';
 import { DatabaseService } from '../tribe-webhook/database.service';
-import { SpaceRoleType } from '@tribeplatform/gql-client/types';
+import {
+  PostMappingTypeEnum,
+  PostTypeContext,
+  SpaceRoleType,
+} from '@tribeplatform/gql-client/types';
 
 @Injectable()
 export class AppService {
   private tribeClient: TribeClient;
   private tribeAccessToken: string;
   private readonly logger = new Logger(DatabaseService.name);
+  private reportSpaceId: ID = '';
+
+  private readonly LEADERBOARD_POST_TITLE = 'Kudos LeaderBoard';
 
   constructor(
     private readonly configService: ConfigService,
@@ -32,18 +39,28 @@ export class AppService {
     return membersId;
   }
 
-  private async createKudosReportSpace(members: ID[]) {
+  private async isKudosSpaceExist(): Promise<ID | null> {
     const spaces = await this.tribeClient.spaces.list({ limit: 10 }, 'basic');
-    const isKudosSpaceExist = spaces.edges.some(
-      (space) =>
-        space.node.name === 'Kudos' && space.node.imageId === ':doughnut:',
-    );
-    if (isKudosSpaceExist) {
-      this.logger.verbose(`Space already created.`);
-      return;
-    }
-    this.tribeClient.spaces
-      .create({
+    let spaceId: ID = null;
+    spaces.edges.some((space) => {
+      // TODO: get from config
+      if (space.node.name === 'Kudos' && space.node.imageId === ':doughnut:') {
+        spaceId = space.node.id;
+        return true;
+      }
+      return false;
+    });
+    return spaceId;
+  }
+
+  private async createKudosReportSpace(members: ID[]): Promise<ID> {
+    try {
+      const spaceId = await this.isKudosSpaceExist();
+      if (spaceId) {
+        this.logger.verbose(`Space already created.`);
+        return spaceId;
+      }
+      const kudosSpace = await this.tribeClient.spaces.create({
         input: {
           name: 'Kudos',
           imageId: ':doughnut:',
@@ -54,25 +71,143 @@ export class AppService {
           whoCanReply: [SpaceRoleType.ADMIN],
           whoCanReact: [SpaceRoleType.MEMBER],
         },
+      });
+      return kudosSpace.id;
+    } catch (error) {
+      this.logger.error(error);
+    }
+  }
+
+  private async setTribeClientToken() {
+    this.tribeAccessToken = await this.tribeClient.generateToken({
+      networkId: this.configService.get('TRIBE_NETWORK_ID'),
+      memberId: this.configService.get('TRIBE_MY_ADMIN_ID'),
+    });
+    this.tribeClient.setToken(this.tribeAccessToken);
+  }
+
+  private async initialKudosApp(): Promise<void> {
+    await this.setTribeClientToken();
+    const membersId = await this.addMembers();
+    this.reportSpaceId = await this.createKudosReportSpace(membersId);
+    this.updateLeaderboardAtKudosSpace(this.reportSpaceId);
+  }
+
+  public getLeaderBoardAsHTML() {
+    const leaderboard = this.databaseService.getLeaderboard();
+    let htmlLeaderboard = '<table>';
+    leaderboard.forEach((item, index) => {
+      htmlLeaderboard += `<tr><td>No.${index + 1}</td><td>${item[0]}</td><td>${
+        item[1]
+      }</td><td>${item[2]}</td></tr>`;
+    });
+    return htmlLeaderboard + '</table>';
+  }
+
+  private getLeaderBoardAsPost() {
+    return {
+      title: JSON.stringify(this.LEADERBOARD_POST_TITLE),
+      content: JSON.stringify(this.getLeaderBoardAsHTML()),
+      previewImageId: '',
+    };
+  }
+
+  private async createLeaderboardPost(
+    spaceId: ID,
+    title: string,
+    content: string,
+  ): Promise<void> {
+    const postTypeId = await this.findPostTypeId();
+    this.tribeClient.posts
+      .create({
+        spaceId,
+        input: {
+          postTypeId, //: postDiscussionType.node.id,
+          mappingFields: [
+            {
+              key: 'title',
+              type: PostMappingTypeEnum.TEXT,
+              value: title,
+            },
+            {
+              key: 'content',
+              type: PostMappingTypeEnum.HTML,
+              value: content,
+            },
+          ],
+          publish: true,
+        },
       })
       .then((result) => {
-        this.logger.verbose(`Space Created: ${result.id}`);
+        this.logger.verbose(`Leaderboard post created! ${result.id}`);
       })
       .catch((error) => {
         this.logger.error(error);
       });
   }
 
-  private async initialKudosApp(): Promise<void> {
-    this.tribeAccessToken = await this.tribeClient.generateToken({
-      networkId: this.configService.get('TRIBE_NETWORK_ID'),
-      memberId: this.configService.get('TRIBE_MY_ADMIN_ID'),
+  private updateLeaderBoardPost(postId: ID, title: string, content: string) {
+    this.tribeClient.posts
+      .update({
+        id: postId,
+        input: {
+          mappingFields: [
+            {
+              key: 'title',
+              type: PostMappingTypeEnum.TEXT,
+              value: title,
+            },
+            {
+              key: 'content',
+              type: PostMappingTypeEnum.HTML,
+              value: content,
+            },
+          ],
+          publish: true,
+        },
+      })
+      .then((result) => {
+        this.logger.verbose(`Leaderboard post updated! ${result.id}`);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  private async findPostTypeId(): Promise<ID> {
+    const postTypes = await this.tribeClient.posts.listPostTypes({
+      limit: 10,
+      context: PostTypeContext.POST,
     });
-    this.tribeClient.setToken(this.tribeAccessToken);
+    const postDiscussionType = postTypes.edges.filter(
+      (edge) => edge.node.context === 'post' && edge.node.name === 'Discussion',
+    )[0];
+    return postDiscussionType.node.id;
+  }
 
-    const membersId = await this.addMembers();
-    this.databaseService.print();
-
-    this.createKudosReportSpace(membersId);
+  private updateLeaderboardAtKudosSpace(spaceId: ID) {
+    const leaderBoardPost = this.getLeaderBoardAsPost();
+    this.tribeClient.posts.list({ limit: 2, spaceId }).then((posts) => {
+      let leaderboardPostId = '';
+      posts.edges.some((edge) => {
+        if (edge.node.title === this.LEADERBOARD_POST_TITLE) {
+          leaderboardPostId = edge.node.id;
+          return true;
+        }
+        return false;
+      });
+      if (!leaderboardPostId) {
+        this.createLeaderboardPost(
+          spaceId,
+          leaderBoardPost.title,
+          leaderBoardPost.content,
+        );
+      }
+      this.updateLeaderBoardPost(
+        leaderboardPostId,
+        leaderBoardPost.title,
+        leaderBoardPost.content,
+      );
+    });
   }
 }
