5e5d2bbbe793d41d29cc375148437cfdfe8b60ab
Mehdi <mohammadmoozeh@gmail.com>
2022-04-15 20:44:45 +0300

    refactor: add getter for name and stats

--- src/tribe-webhook/kudos.model.ts
+++ src/tribe-webhook/kudos.model.ts
@@ -1,16 +1,20 @@
 export class KudosModel {
   private member: ID;
-  private name: string;
   private dayCapacity: number;
   private remainingKudos: number;
   private given: { [member: ID]: number }[] = [];
   private received: { [member: ID]: number }[] = [];
+  private _name: string;
+  private _totalReceived: number;
+  private _totalGiven: number;
 
   constructor(member: ID, name: string) {
     this.member = member;
-    this.name = name;
     this.dayCapacity = 5;
     this.remainingKudos = this.dayCapacity;
+    this._name = name;
+    this._totalReceived = 0; // Math.floor(Math.random() * 30);
+    this._totalGiven = 0;
   }
 
   resetRemainingKudos() {
@@ -18,15 +22,29 @@ export class KudosModel {
   }
 
   give(receivers: ID[], count: number): number | Error {
-    const totalGiven = receivers.length * count;
-    if (totalGiven > this.remainingKudos)
+    const neededKudos = receivers.length * count;
+    if (neededKudos > this.remainingKudos)
       throw new Error('You dont have enough Kudos!');
     receivers.forEach((receiver) => this.given.push({ [receiver]: count }));
-    this.remainingKudos -= totalGiven;
-    return totalGiven;
+    this.remainingKudos -= neededKudos;
+    this._totalGiven += neededKudos;
+    return neededKudos;
   }
 
   receive(from: ID, count: number) {
     this.received.push({ [from]: count });
+    this._totalReceived += count;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  get totalReceived(): number {
+    return this._totalReceived;
+  }
+
+  get totalGiven(): number {
+    return this._totalGiven;
   }
 }
